9e74088ecfbda45550bba12642ff2d7a482b31ae
Yoav Abadi <63541874+Yoas1@users.noreply.github.com>
2022-11-02 22:08:12 +0200

    add try except

--- ssh_connection.py
+++ ssh_connection.py
@@ -9,7 +9,17 @@ password = "password"
 
 client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(host, username=username, password=password)
-_stdin, _stdout, _stderr = client.exec_command(command)
-print(_stdout.read().decode())
-client.close()
+try:
+    client.connect(host, username=username, password=password)
+    _stdin, _stdout, _stderr = client.exec_command(command)
+    print(_stdout.read().decode())
+    client.close()
+
+
+except paramiko.ssh_exception.NoValidConnectionsError:
+    print("Unable to connect to port 22 on " + host)
+
+
+except:
+    print("Error")
+
